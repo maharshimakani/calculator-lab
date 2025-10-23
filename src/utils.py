05e8bec6102d7a400994357a53a9b11f62351dd3
Maharshi Patel <pmaharshi999@gmail.com>
2025-10-23 11:40:03 +0200

    feat(utils): implement add() and subtract()

---
+++ src/utils.py
@@ -0,0 +1,22 @@
+def add(*args):
+    """
+    Return the sum of all numeric arguments.
+    If no arguments are provided, return 0.
+    """
+    total = 0
+    for x in args:
+        total += x
+    return total
+
+
+def subtract(*args):
+    """
+    Subtract all subsequent args from the first one.
+    If no arguments are provided, return 0.
+    """
+    if not args:
+        return 0
+    result = args[0]
+    for x in args[1:]:
+        result -= x
+    return result
